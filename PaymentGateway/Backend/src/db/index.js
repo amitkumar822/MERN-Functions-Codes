35eb52d34c22ecd4f3380c6d1007cbf02e3b5f06
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-11-26 11:35:16 +0530

    fex some issue

--- PaymentGateway/Backend/src/db/index.js
+++ PaymentGateway/Backend/src/db/index.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error);
